0d0621dda8bba780f7e69ebbfce8c1c03e61395b
Ryan Patel <rajanpatel1011@gmail.com>
2018-03-20 23:02:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function nowServing(katzDeli){
   if(katzDeli.length===0){
     return "There is nobody waiting to be served!";
   }
-num = 0;
+num = 1;
     var temp = katzDeli[num];
     katzDeli.pop();
     num--;
