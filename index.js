49488cf0491988f583ee2d7dc8581c8c762c1dc6
Ryan Patel <rajanpatel1011@gmail.com>
2018-03-20 22:11:32 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,35 @@
+var katzDeli = [];
+var num =0 ;
+
+function takeANumber(katzDeli,name){
+
+
+katzDeli.push(name);
+num++;
+return `Welcome, ${katzDeli[num-1]}. You are number ${num} in line.`;
+
+}
+/*
+function nowServing(katzDeli){
+  if(katzDeli.length===0){
+    return "There is nobody waiting to be served!";
+  }
+  var temp = katzDeli[num];
+  katzDeli.pop();
+  num--;
+  
+  return `Currently serving ${temp}.`;
+}
+function currentLine(katzDeli){
+  var newArray=[];
+  if(katzDeli===isNaN){
+   return 'The line is currently empty.'; 
+  }else{
+  for(var i = 1;i<katzDeli.length+1;i++){
+    newArray[i] = ` ${i}. ${katzDeli[i-1]}`;
+  }
+    return `The line is currently:${newArray}.`;
+  }
+
+}
+*/
